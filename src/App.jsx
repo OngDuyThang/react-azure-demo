4d2a4e3892277c1e55a808ccd38157104c8173ee
OngDuyThang <duythangong@gmail.com>
2023-05-16 13:15:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import './App.scss';
+
+export default function App() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [res, setRes] = useState()
+  async function submit() {
+    try {
+      const res = await axios.get('https://ongduythangspring.azurewebsites.net/login', { userName, password })
+      setRes(res.data)
+    } catch (err) {
+      console.log('Login failure!')
+    }
+  }
+
+  return (
+    <div className="appContainer">
+      <h2>Login form</h2>
+      <input type="text" onChange={(e) => setUserName(e.target.value)} placeholder="User Name" />
+      <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <div className="btn" onClick={() => submit()}>Submit</div>
+      <span>{res}</span>
+    </div>
+  )
+}
